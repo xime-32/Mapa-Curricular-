3d25829ed82b1c483f5dd297d65593951181e9d4
xime-32 <ximenalara464@gmail.com>
2025-07-13 23:21:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function mostrarInfo(elem) {
-  const info = elem.querySelector(".info");
-  info.style.display = info.style.display === "block" ? "none" : "block";
+function mostrarInfo(elemento) {
+  const info = elemento.querySelector('.info');
+  info.style.display = info.style.display === 'block' ? 'none' : 'block';
+
+  // Marcar como completada al hacer doble clic
+  if (elemento.classList.contains('completada')) {
+    elemento.classList.remove('completada');
+  } else {
+    elemento.classList.add('completada');
+  }
 }
